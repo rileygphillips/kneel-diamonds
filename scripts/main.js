7d915f70ace06a4d267eaa9c69049a602740be42
Riley Phillips <rpdeveloping@gmail.com>
2022-02-09 09:42:55 -0600

    final draft

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,8 @@ const renderAllHTML = () => {
 
 renderAllHTML()
 
+document.addEventListener("stateChanged", event => {
+    console.log("State of data has changed. Regenerating HTML...")
+    renderAllHTML()
+})
+
